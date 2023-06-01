20d573f95d69f049a7c49a504c16b83dee81e913
anna <annakoung6119@gmail.com>
2023-06-01 09:26:47 +0900

    2023년 5월달 파일, wireframe 파일 추가

---
+++ 2023년_5월달/23.05.09/web/js/main.js
@@ -0,0 +1,10 @@
+setInterval(function () {
+  $(".img_slide ul").delay(2500);
+  $(".img_slide ul").animate({ marginLeft: "-1500px" });
+  $(".img_slide ul").delay(2500);
+  $(".img_slide ul").animate({ marginLeft: "-3000px" });
+  $(".img_slide ul").delay(2500);
+  $(".img_slide ul").animate({ marginLeft: "-4500px" });
+  $(".img_slide ul").delay(2500);
+  $(".img_slide ul").animate({ marginLeft: "0px" });
+});
---
+++ 2023년_5월달/23.05.19/wireframe/js/img_slide.js
@@ -0,0 +1,8 @@
+setInterval(function () {
+  $(".img_slide ul").delay(2500);
+  $(".img_slide ul").animate({ marginLeft: "-800px" });
+  $(".img_slide ul").delay(2500);
+  $(".img_slide ul").animate({ marginLeft: "-1600px" });
+  $(".img_slide ul").delay(2500);
+  $(".img_slide ul").animate({ marginLeft: "0px" });
+});
